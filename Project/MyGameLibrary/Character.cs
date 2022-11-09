81cde55a8006fbe1ee3cf1ed9a3c98ba2fac9d59
brendan <brendanguillory@gmail.com>
2022-11-09 16:01:30 -0600

    Enemies can now be hit by cars

--- Project/MyGameLibrary/Character.cs
+++ Project/MyGameLibrary/Character.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 
 namespace Fall2020_CSC403_Project.code {
   public class Character {
-    private const int GO_INC = 3;
+    private int Go_Inc = 3;
+    
+    private const int CarGo_Inc = 5;
 
-    public Vector2 MoveSpeed { get; set; }
+    public Vector2 MoveSpeed { get; private set; }
     public Vector2 LastPosition { get; private set; }
     public Vector2 Position { get; private set; }
     public Collider Collider { get; set; }
+    public Boolean InCar { get; set; }
+
+    public void BoardCar() {
+      Go_Inc = CarGo_Inc;
+      InCar = true;
+    }
 
     public Character(Vector2 initPos, Collider collider) {
       Position = initPos;
       Collider = collider;
+      InCar = false;
     }
 
     public void Move() {
@@ -29,7 +38,7 @@ namespace Fall2020_CSC403_Project.code {
     }
 
     public void GoVector(Vector2 MoveDir) {
-      MoveSpeed = new Vector2(GO_INC * MoveDir.x, GO_INC * MoveDir.y);
+      MoveSpeed = new Vector2(Go_Inc * MoveDir.x, Go_Inc * MoveDir.y);
     }
 
     public void ResetMoveSpeed() {
