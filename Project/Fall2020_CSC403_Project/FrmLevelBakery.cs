03ae0b4bc796fd032a1d3f8a76d9a09f188b06e2
brendan <brendanguillory@gmail.com>
2022-11-09 16:01:08 -0600

    Added references to player car images to levels

--- Project/Fall2020_CSC403_Project/FrmLevelBakery.cs
+++ Project/Fall2020_CSC403_Project/FrmLevelBakery.cs
@@ -24,7 +24,9 @@ namespace Fall2020_CSC403_Project {
     private bool isPaused;
     private Stopwatch timer;
     private Tuple<Key, Vector2>[] KeyBindings;
-    private FrmPause frmPause; 
+    private FrmPause frmPause;
+
+    private Image playerCarImg;
 
     public FrmLevelBakery() {
       InitializeComponent();
@@ -52,6 +54,8 @@ namespace Fall2020_CSC403_Project {
       bossKoolaid.Color = Color.Red;
       enemyPoisonPacket.Color = Color.Green;
       enemyCheeto.Color = Color.FromArgb(255, 245, 161);
+
+      playerCarImg = global::Fall2020_CSC403_Project.Properties.Resources.carBakeryCharacter;
 
       walls = new Character[NUM_WALLS];
       for (int w = 0; w < NUM_WALLS; w++) {
@@ -138,6 +142,7 @@ namespace Fall2020_CSC403_Project {
 
     private void boardCar() {
       removeEnemy(car);
+      player.BoardCar();
     }
 
     private bool HitAWall(Character c) {
